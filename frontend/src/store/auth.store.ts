414d4dc78cf57f058fbbaad3514886b50be04485
Lazar Conic <lazarconic@yahoo.com>
2026-01-25 21:04:05 +0100

    Setup tanstack.

--- frontend/src/store/auth.store.ts
+++ frontend/src/store/auth.store.ts
@@ -4,9 +4,9 @@ import type { User } from "../types";
 import { login, register } from "../api/auth.api";
 
 export const useAuthStore = create((set) => ({
-    user: null,
-    token: null,
-    isAuthenticated: false,
-    setAuth: (user, token) => set({ user, token, isAuthenticated: true }),
-    clearAuth: () => set({ user: null, token: null, isAuthenticated: false }),
+  user: null,
+  token: null,
+  isAuthenticated: false,
+  setAuth: (user, token) => set({ user, token, isAuthenticated: true }),
+  clearAuth: () => set({ user: null, token: null, isAuthenticated: false }),
 }));
